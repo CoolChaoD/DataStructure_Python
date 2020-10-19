45bc3044ecd5c3483bf62bb02281351bb4c9e2a3
dongchao <3291240985@qq.com>
2020-10-19 21:27:42 +0800

    完成了冒泡算法的优化

--- DataStructure_Python/sort_myself/bubble_sort.py
+++ DataStructure_Python/sort_myself/bubble_sort.py
@@ -2,15 +2,23 @@
 # author=ChaoD
 # date:2020/10/19 20:43
 
+
 def bubbleSort(arr):
+    flag = 0  # 标志位用来检测某一轮是否发生了交换
     n = len(arr)
     for i in range(n - 1):
         for j in range(n - i - 1):
             if arr[j] > arr[j + 1]:
+                flag = 1
                 temp = arr[j]
                 arr[j] = arr[j + 1]
                 arr[j + 1] = temp
-            print("第%d轮%d次" % (i + 1, j+1), arr)
+        if flag == 0:
+            break
+        else:
+            flag = 0
+
+        print("第%d轮" % (i + 1), arr)
 
 
 if __name__ == '__main__':
